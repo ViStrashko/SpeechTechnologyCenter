03fda9a834f84f0d2057743b4c08542ff1437937
ViStrashko <voringir@mail.ru>
2023-02-07 21:14:36 +0300

    Initial

---
+++ TestProject/Support/Urls.cs
@@ -0,0 +1,8 @@
+﻿namespace TestProject.Support
+{
+    public class Urls
+    {
+        public const string Host = $"http://localhost:5000";
+        public const string UrlPath = $"{Host}/api/books";
+    }
+}
